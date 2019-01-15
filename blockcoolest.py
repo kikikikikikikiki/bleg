27de93525e95ba6f8d0d24b76c2aaebd5c8c593a
kikikikikikikiki <29565981+kikikikikikikiki@users.noreply.github.com>
2019-01-15 22:17:25 +0000

    Add files via upload
    Added FFT, fixed heading, looks good

--- blockcoolest.py
+++ blockcoolest.py
@@ -4,11 +4,13 @@ import pyaudio
 import numpy
 import time
 import wave
+import scipy.fftpack
+
 from video import make_video
 pygame.init()   
 
 surface_size = 1000
-main_surface = pygame.display.set_mode((900,900))
+main_surface = pygame.display.set_mode((600,600))
 my_clock = pygame.time.Clock()
 
 
@@ -16,8 +18,8 @@ def draw_tree(inord, order, theta, thetab, sz, posn, heading, color=(0,0,0), dep
 
    trunk_ratio = 0.01618       
    trunk = sz * trunk_ratio
-   delta_x = trunk * math.cos((heading*pygame.mouse.get_pos()[0]/400).imag)
-   delta_y = trunk * math.sin((heading*1j*pygame.mouse.get_pos()[1]/400).real)
+   delta_x = trunk * math.cos((heading*400/400).imag)
+   delta_y = trunk * math.sin((heading*1j*400/400).real)
 
    thetaj = theta*1j*1j
    thetai = thetab*1j*1j
@@ -30,19 +32,19 @@ def draw_tree(inord, order, theta, thetab, sz, posn, heading, color=(0,0,0), dep
       #pygame.draw.line(main_surface, color, newpos, posn, order)
       pygame.draw.circle(main_surface, color, (int(posn[0]),int(posn[1])),1,1)
       pygame.draw.circle(main_surface, color, (int(newpos[0]),int(newpos[1])),1,1)
-      pygame.draw.circle(main_surface, color, (int(posn[0]),int(newpos[1])),1,1)
-      pygame.draw.circle(main_surface, color, (int(newpos[0]),int(posn[1])),1,1)
+      #pygame.draw.circle(main_surface, color, (int(posn[0]),int(newpos[1])),1,1)
+      #pygame.draw.circle(main_surface, color, (int(newpos[0]),int(posn[1])),1,1)
       
    #gradd  = 128+(127*math.cos((math.exp(heading.imag)).imag))
-   gradd  = 128+(127*math.cos((heading*pygame.mouse.get_pos()[0]/400).imag))
-   gradd2 = 128+(128*math.sin((heading*1j*pygame.mouse.get_pos()[1]/400).real))
+   gradd  = 128+(127*math.cos((heading*1).imag))
+   gradd2 = 128+(127*math.sin((heading*1j*1).real))
    gradd3 = 128+(128*math.sin((heading).real))
    
 
    if order > 0:
       if depth == 0:
-          color1 = (0,gradd,gradd2)
-          color2 = (255,gradd,gradd2)
+          color1 = (gradd,0,gradd2)
+          color2 = (gradd,0,gradd2)
       else:
           color1 = (0,gradd,gradd2)
           color2 = (0,gradd,gradd2)#(gradd,gradd2,0)
@@ -58,6 +60,7 @@ def gameloop():
     theta2 = 0
     frame = 0
     inord = 0
+    headingin = 0
    
     WIDTH = 4
     CHANNELS = 1
@@ -65,25 +68,27 @@ def gameloop():
 
     save_screen = make_video(main_surface)
     
-    wf = wave.open('what4.wav', 'rb')
+    wf = wave.open('sim.wav', 'rb')
     p = pyaudio.PyAudio()
     
     
-    
+   
     def callback(in_data, frame_count, time_info, status):
        pygame.display.flip()
-       my_clock.tick(24)
+       my_clock.tick(20)
        next(save_screen)
        data = wf.readframes(frame_count)
        decoded = numpy.fromstring(data, dtype=numpy.int16)
+       yf = scipy.fftpack.fft(decoded)
+       #print(yf)
        #here = numpy.fft(
        #theta1 = 5+(math.log10((1/math.fabs(0.00001+decoded[0])))*1j)
        #theta2 = 5+(math.log10((1/math.fabs(0.00001+decoded[1])))*1j)
 
        #theta1 = (math.exp(math.sin(decoded[0])))*1j
        #theta2 = (math.exp(math.sin(decoded[1])))*1j
-        
-              
+       decoded_oh = decoded[::2]
+       yf_oh = yf[::2]       
        
        
        #theta1 = decoded[0]
@@ -93,15 +98,20 @@ def gameloop():
        if math.fabs(decoded[0]) > 0:
            if math.fabs(decoded[1]) > 0:
                #print(decoded[0])
-               for x in decoded:
-                  theta1 = math.sin((math.exp((math.fabs(x)/6000)*1 )))*1j
-                  theta2 = math.sin((math.exp((math.fabs(x)/6000)*1)))*1j
-                  draw_tree(inord, 2, theta1, theta2, surface_size*2, (450,450), math.pi)
+               for i, j in zip(decoded_oh, yf_oh): # Change to every other value for left/right side [0] = left [1] = right
+                  theta1 = math.cos((math.exp(((i)/6000)*1 )))*1j
+                  theta2 = math.cos((math.exp(((i)/6000)*1)))*1j
+
+                  #theta1 = ((math.exp(((i)/6000)*1 )))*1j
+                  #theta2 = ((math.exp(((i)/6000)*1)))*1j
+
+                  #theta1 = math.cos((math.exp((math.fabs(i)/6000)*1 )))*1j
+                  #theta2 = math.cos((math.exp((math.fabs(i)/6000)*1)))*1j
+                  
+                  draw_tree(inord, 6, theta1, theta2, surface_size*3, (300,300), math.pi*j)
        #pygame.display.flip()
-       if math.fabs(decoded[0])>4e-05:
-          if math.fabs(decoded[1])>4e-05:
              #draw_tree(inord, 8, theta1, theta2, surface_size*0.8, (400,400), math.pi)
-             pass
+             #pass
        
        
        #print(decoded)
